27614cc17a760d9d2a0d5a570164c530ea044e44
sarwar151 <116275900+sarwar151@users.noreply.github.com>
2022-10-21 09:36:11 +0530

    Create Importpackage.java

---
+++ Importpackage.java
@@ -0,0 +1,8 @@
+import mypackage.Demo; 
+ public class ImportPackage { 
+     public static void main(String[] args) { 
+         Demo d1=new Demo(); 
+         System.out.print("I am imported from "); 
+         d1.display(); 
+     } 
+ }
