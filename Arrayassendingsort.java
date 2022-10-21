337e7dccb64b200d8a16086b3b4cde347674b2f1
sarwar151 <116275900+sarwar151@users.noreply.github.com>
2022-10-21 09:43:26 +0530

    Create Arrayassendingsort.java

---
+++ Arrayassendingsort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; 
+ public class ArrayAscendingSort { 
+     public static void main(String[] args) { 
+         int[] array=new int[10]; 
+         int i; 
+         System.out.println("Enter the elements of the array"); 
+         for ( i = 0; i < 10; i++) { 
+             Scanner s=new Scanner(System.in); 
+             array[i]=s.nextInt(); 
+         } 
+         System.out.print("Original array = "); 
+         for (i = 0; i < 10; i++) { 
+             System.out.print(array[i] + " "); 
+         } 
+         for ( i = 0; i < 10; i++) { 
+             for (int j = i+1; j < 10; j++) { 
+                 int temp=0; 
+                 if(array[i]>array[j]){ 
+                     temp=array[i]; 
+                     array[i]=array[j]; 
+                     array[j]=temp; 
+                 } 
+             } 
+         } 
+         System.out.println(""); 
+         System.out.print("Array in ascending order = "); 
+         for ( i = 0; i < 10; i++) { 
+             System.out.print(array[i] + " "); 
+         } 
+  
+     } 
+ }
